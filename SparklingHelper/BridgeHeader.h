c2b09c88ade0891a70a518f8b6972b75be1bedce
mzp <mzpppp@gmail.com>
2015-07-16 23:23:22 +0900

    :apple: Add Xcode's framework and its headers
    This is private and undocumented frameworks.
    So I create header by class-dump command(http://stevenygard.com/projects/class-dump/).

---
+++ SparklingHelper/BridgeHeader.h
@@ -0,0 +1,16 @@
+//
+//  BridgeHeader.h
+//  SparklingHelper
+//
+//  Created by mzp on 7/16/15.
+//  Copyright © 2015 mzp. All rights reserved.
+//
+
+#ifndef BridgeHeader_h
+#define BridgeHeader_h
+
+#import <GCDWebServer.h>
+#import <GCDWebServerDataResponse.h>
+#import "DVTFoundation.h"
+
+#endif /* BridgeHeader_h */
